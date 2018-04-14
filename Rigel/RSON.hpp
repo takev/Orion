a54fd7869b1f31a04e25352a0e0e496166bcd333
Tjienta Vara <tjientavara@users.noreply.github.com>
2018-04-14 16:24:26 +0200

    Add constants.

---
+++ Rigel/RSON.hpp
@@ -0,0 +1,19 @@
+
+namespace Orion {
+namespace Rigel {
+namespace RSON {
+
+const char MARK_CODE = 0x00;
+const char NONE_CODE = 0x10;
+const char TRUE_CODE = 0x11;
+const char FALSE_CODE = 0x12;
+const char LIST_CODE = 0x13;
+const char OBJECT_CODE = 0x14;
+const char DECIMAL_FLOAT_CODE = 0x15;
+const char BINARY_FLOAT_CODE = 0x16;
+const char BYTE_ARRAY_CODE = 0x17;
+const char UTF8_STRING_CODE = 0x18;
+const char RESERVED1_CODE = 0x19;
+const char RESERVED2_CODE = 0x1a;
+
+};};};
